e10ed707988391a4584d71766363786539ab7de3
Milan Roka <101684566+MilanRoka@users.noreply.github.com>
2023-08-18 18:03:06 +0545

    "few ui modification"

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,41 +1,71 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { TbPhotoSearch } from 'react-icons/tb';
 
 const Search = () => {
-
-
   const [searchTerm, setSearchTerm] = useState('');
-  const [images, setImages] = useState([
-    // You can add your predefined images here
-    { id: 1, urls: { small: '/img1.jpg' }, alt_description: 'Placeholder 1' },
-    { id: 2, urls: { small: '/bg1.jpg' }, alt_description: 'Placeholder 2' },
-    { id: 3, urls: { small: '/bg1.jpg' }, alt_description: 'Placeholder 3' },
-    { id: 4, urls: { small: '/bg1.jpg' }, alt_description: 'Placeholder 4' },
-    { id: 5, urls: { small: '/bg1.jpg' }, alt_description: 'Placeholder 5' },
-    { id: 6, urls: { small: '/bg1.jpg' }, alt_description: 'Placeholder ' },
-    { id: 7, urls: { small: '/bg1.jpg' }, alt_description: 'Placeholder ' },
-    { id: 8, urls: { small: '/bg1.jpg' }, alt_description: 'Placeholder ' },
-    { id: 9, urls: { small: '/bg1.jpg' }, alt_description: 'Placeholder ' },
-    { id: 10, urls: { small: '/bg1.jpg' }, alt_description: 'Placeholder 5' },
-    // Add more placeholders if needed
-  ]);
+  const [images, setImages] = useState([]);
+  const [showLoadMoreButton, setShowLoadMoreButton] = useState(false);
+
+  const [currentPage, setCurrentPage] = useState(1);
 
   const backgroundImageStyle = {
     backgroundImage: "url('/bg2.jpg')",
     backgroundPosition: 'center',
   };
 
+  useEffect(() => {
+    fetchInitialImages();
+  }, []);
+
+  const fetchInitialImages = async () => {
+    try {
+      const response = await axios.get('https://api.unsplash.com/photos', {
+        params: {
+          per_page: 10, // Number of initial images
+          client_id: '7Gli5Kqd_xGlW5hWkrt7fzbF9z4UJ6v6TjhcrLYHUVE', 
+        },
+      });
+      setImages(response.data);
+    } catch (error) {
+      console.error('Error fetching initial images:', error);
+    }
+  };
+
   const searchImages = async () => {
     try {
       const response = await axios.get('https://api.unsplash.com/search/photos', {
         params: {
-          query: `${searchTerm}`,
-          per_page: 15,
-          client_id: '7Gli5Kqd_xGlW5hWkrt7fzbF9z4UJ6v6TjhcrLYHUVE', // Replace with your access key
+          query: searchTerm,
+          per_page: 10,
+          client_id: '7Gli5Kqd_xGlW5hWkrt7fzbF9z4UJ6v6TjhcrLYHUVE', 
         },
       });
       setImages(response.data.results);
+      setCurrentPage(1);
+      setShowLoadMoreButton(true);
+    } catch (error) {
+      console.error('Error fetching images:', error);
+    }
+  };
+
+  const loadMoreImages = async () => {
+    try {
+      const nextPage = currentPage + 1;
+      const response = await axios.get('https://api.unsplash.com/search/photos', {
+        params: {
+          query: searchTerm,
+          per_page: 10,
+          page: nextPage,
+          client_id: '7Gli5Kqd_xGlW5hWkrt7fzbF9z4UJ6v6TjhcrLYHUVE', // Replace with your Unsplash API access key
+        },
+      });
+      setImages((prevImages) => [...prevImages, ...response.data.results]);
+      setCurrentPage(nextPage);
+
+      if (response.data.results.length === 0) {
+        setShowLoadMoreButton(false);
+      }
     } catch (error) {
       console.error('Error fetching images:', error);
     }
@@ -45,34 +75,12 @@ const Search = () => {
     e.preventDefault();
     searchImages();
   };
-  const [showLoadMoreButton, setShowLoadMoreButton] = useState(false);
 
   const toggleLoadMoreButton = () => {
     setShowLoadMoreButton(!showLoadMoreButton);
   };
-  // Define an asynchronous function named loadMoreImages
-  const loadMoreImages = async () => {
-    try {
-      // Attempt to fetch images from the Unsplash API
-      // using the Axios library for making HTTP requests
 
-      // Send a GET request to the Unsplash API's search/photos endpoint
-      const response = await axios.get('https://api.unsplash.com/search/photos', {
-        // Configure parameters for the request
-        params: {
-          query: `${searchTerm}`, // The search term for the images
-          per_page: 10, // Number of images per page
-          client_id: '7Gli5Kqd_xGlW5hWkrt7fzbF9z4UJ6v6TjhcrLYHUVE', // Your Unsplash API access key
-        },
-      });
-      let newArray = [...images];
-      newArray = newArray.concat(response.data.results);
-      setImages(newArray);
-    } catch (error) {
-      // Log an error if the request fails
-      console.error('Error fetching images:', error);
-    }
-  }
+  
   return (
     <div>
       <div className='flex flex-col items-center min-h-full' style={backgroundImageStyle}>
@@ -102,7 +110,13 @@ const Search = () => {
             key={image.id}
             src={image.urls.small}
             alt={image.alt_description}
-            className="m-2 h-80 w-80 object-cover rounded"
+            className="m-2 h-80 w-80 object-cover rounded 
+            hover:opacity-50 transition ease-in-out duration-150"
+            onClick={
+              () => {
+                window.open(image.urls.regular, '_blank');
+              }
+            }
           />
         ))}
       </div>
@@ -112,7 +126,7 @@ const Search = () => {
             onClick={
               loadMoreImages
             }
-            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+            className="px-4 py-2 mb-10 bg-blue-500 text-white rounded hover:bg-blue-600"
           >
             Load More
           </button>
